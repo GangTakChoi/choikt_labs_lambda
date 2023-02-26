a1ce989918f103704b86828e87d39519248bcd00
ChoiKT <chlrkdxkr@gmail.com>
2023-02-26 23:15:47 +0900

    test

--- src/routes/index.router.ts
+++ src/routes/index.router.ts
@@ -5,7 +5,7 @@ const router = express.Router();
 // define the home page route
 router.get("/", (req, res, nest) => {
   console.log("index router");
-  res.send({ Output: "Choikt Labs Api 2" });
+  res.send({ Output: "Choikt Labs Api" });
 });
 
 export default router;
